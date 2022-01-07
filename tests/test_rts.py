094964642432517ad5ec692f174638faf51c2bca
Daniele Tentoni <d.tentoni@wedoit.io>
2022-01-07 15:03:23 +0100

    test(core): add first test file
    Inside that file there are examples about unittests with pytest using fixtures. Following tutorial on [Real Python](https://realpython.com/pytest-python-testing/).

--- tests/test_rts.py
+++ tests/test_rts.py
@@ -1,5 +1,33 @@
+import pytest
+
+from rts.tower import Tower
+
+from rts.constants import (
+  TOWER_WIDTH,
+  TOWER_HEIGHT,
+)
+
 from rts import __version__
 
+@pytest.fixture
+def example_key_data(pressed_key):
+  def helper():
+    tmp = [0] * 300
+    tmp[pressed_key] = 1
+    return tmp
+  return helper
+
+TOWER_STARTING_X = 10
+TOWER_STARTING_Y = 10
+
+@pytest.fixture
+def create_simple_tower():
+  return 1
 
 def test_version():
-    assert __version__ == '0.1.0'
+  assert __version__ == '0.1.0'
+
+def test_tower_initial_position():
+  tower = Tower(TOWER_STARTING_X, TOWER_STARTING_Y)
+  assert tower.rectangle.top == TOWER_STARTING_Y
+  assert tower.rectangle.left == TOWER_STARTING_X
