c9b44e9d87965b93a28a236974851f247810c8d7
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2022-01-07 17:26:51 +0100

    ref(tests): refactoring tests

--- tests/test_rts.py
+++ tests/test_rts.py
@@ -1,33 +1,18 @@
 import pytest
 
-from rts.tower import Tower
-
-from rts.constants import (
-  TOWER_WIDTH,
-  TOWER_HEIGHT,
-)
-
 from rts import __version__
-
-@pytest.fixture
-def example_key_data(pressed_key):
-  def helper():
-    tmp = [0] * 300
-    tmp[pressed_key] = 1
-    return tmp
-  return helper
+from rts.tower import Tower
 
 TOWER_STARTING_X = 10
 TOWER_STARTING_Y = 10
 
 @pytest.fixture
-def create_simple_tower():
-  return 1
+def create_simple_tower() -> Tower:
+  return Tower(TOWER_STARTING_X, TOWER_STARTING_Y)
 
 def test_version():
   assert __version__ == '0.1.0'
 
-def test_tower_initial_position():
-  tower = Tower(TOWER_STARTING_X, TOWER_STARTING_Y)
-  assert tower.rectangle.top == TOWER_STARTING_Y
-  assert tower.rectangle.left == TOWER_STARTING_X
+def test_tower_initial_position(create_simple_tower: Tower):
+  assert create_simple_tower.rect.top == TOWER_STARTING_Y
+  assert create_simple_tower.rect.left == TOWER_STARTING_X
