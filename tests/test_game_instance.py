a1b4bf2bf2089ca01d6f14068496fdcff12e22cc
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2022-01-10 18:20:57 +0100

    fix(instance): game instance doesn't invoke display anymore
    Importing game instance in pytest file doesn't show up display anymore. This let us to test in Github Action without issues. See https://github.com/Daniele-Tentoni/rts/runs/4763845085 for a report in Github Actions about this problem.

--- tests/test_game_instance.py
+++ tests/test_game_instance.py
@@ -1,20 +1,27 @@
+import pygame
 import pygame.sprite
 import pytest
-import rts.rts
+
+from rts.rts import GameInstance
 
 @pytest.fixture
-def sample_game_instance() -> rts.rts.GameInstance:
-  return rts.rts.GameInstance()
+def sample_game_instance() -> GameInstance:
+  pygame.init()
+  pygame.font.init()
+  return GameInstance(pygame.Surface((1, 1)))
 
-def test_add_sprite_to_group(sample_game_instance: rts.rts.GameInstance):
+def test_add_sprite_to_group(sample_game_instance: GameInstance):
   """Test Add Sprite To Group in Game Instance.
 
   When invoking add sprite to group in a game instance, that sprite has to be
   added to GameInstance.all_sprites group too.
 
   Args:
-    sample_game_instance (rts.rts.GameInstance): sample GameInstance.
+    sample_game_instance (GameInstance): sample GameInstance.
   """
   test_sprite: pygame.sprite.Sprite = pygame.sprite.Sprite()
+  test_group = pygame.sprite.Group()
+  test_group.add(test_sprite)
+  assert test_group.has(test_sprite)
   sample_game_instance.add_sprite_to(test_sprite, sample_game_instance.towers)
   assert sample_game_instance.all_sprites.has(test_sprite)
