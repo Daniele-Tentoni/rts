73b0706ab869d63c0475f0b940da1bb3a4c51a75
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2022-04-27 18:07:55 +0200

    fix: use new MessageBox

--- rts/game.py
+++ rts/game.py
@@ -16,6 +16,7 @@ from rts.config import *
 from rts.models.game_entity import GameEntity
 from rts.controllers.entity_controller import EntityController
 from rts.controllers.event_controller import EventController
+from rts.models.message_box import MessageBox
 from rts.sprites.ruler import Ruler
 from rts.sprites.soldier import Soldier
 from rts.sprites.tower import Tower
@@ -28,6 +29,8 @@ class Game:
   # Reference to the controllers
   entity_controller: EntityController
   event_controller: EventController
+
+  message_box: MessageBox
 
   # Screen properties used during rendering
   screen: Surface
@@ -67,6 +70,7 @@ class Game:
     # Initialization of the main entities
     self.init_rulers()
     self.init_towers()
+    self.init_messages()
 
   # Generates the rulers of the game
   def init_rulers(self) -> None:
@@ -126,6 +130,10 @@ class Game:
       self.entity_controller.register_entity(tower)
       self.event_controller.register_time_callback(add_soldiers, tower.create_soldiers)
 
+  def init_messages(self) -> None:
+    self.message_box = MessageBox()
+    self.entity_controller.register_entity(self.message_box)
+
   # Keeps computing and showing frames until exit is requested
   def game_loop(self) -> None:
 
@@ -144,10 +152,12 @@ class Game:
     while self.running:
       # Screen cleaning
       self.screen.fill(SCREEN_COLOR)
+      self.message_box.reset()
 
       # System label rendering
-      label = self.sys_font.render("To conquer enemy towers, send them soldiers from your towers.", 1, TEXT_COLOR)
-      self.screen.blit(label, (80, 40))
+      self.message_box.append("To conquer enemy towers, send them soldiers from your towers.")
+      self.message_box.append("Do you like this message box?")
+      self.message_box.append("Now I can write some arbitrary long (not well wrapped) messages inside it ;)")
 
       #TODO: Compute DELTA_TIME
 
@@ -166,7 +176,14 @@ class Game:
         towers = self.entity_controller.entity_dict[Tower]
         for tower in towers:
           tower.update()
+          tower.update_soldiers_label()
           self.screen.blit(tower.surf, tower.rect)
+
+          rect = tower.print_rect()
+          s_rect = draw.rect(self.screen, TEXT_COLOR, rect, border_radius=2)
+          mouse_pos = mouse.get_pos()
+          if s_rect.right > mouse_pos[0] > s_rect.left and s_rect.bottom > mouse_pos[1] > s_rect.top:
+            self.message_box.append("Tower soldier generation.")
 
       # Route updates
       if self.tower_traced is not None:
@@ -193,6 +210,11 @@ class Game:
         for ruler in rulers:
           ruler.update()
           self.screen.blit(ruler.surf, ruler.rect)
+
+      # Messages update
+      if self.message_box.has_messages():
+        self.message_box.update()
+        self.screen.blit(self.message_box.surf, self.message_box.rect)
 
       # Renders the scene
       display.flip()
