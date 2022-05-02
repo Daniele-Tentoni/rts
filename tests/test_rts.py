9b6e2a905aea94e25dc74020373125bae7d22d81
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2022-05-03 00:14:13 +0200

    ref: run black on test suite

--- tests/test_rts.py
+++ tests/test_rts.py
@@ -6,6 +6,7 @@ import rts.sprites.tower
 TOWER_STARTING_X = 10
 TOWER_STARTING_Y = 10
 
+
 @pytest.fixture
 def create_simple_tower() -> rts.sprites.tower.Tower:
-  return rts.sprites.tower.Tower(TOWER_STARTING_X, TOWER_STARTING_Y)
+    return rts.sprites.tower.Tower(TOWER_STARTING_X, TOWER_STARTING_Y)
