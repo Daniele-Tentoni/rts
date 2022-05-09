b8ff388a2db229e0717769ce859942ed06890f79
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2022-05-10 00:24:30 +0200

    ref: use MetaSingleton in each controller

--- rts/controllers/entity_controller.py
+++ rts/controllers/entity_controller.py
@@ -1,34 +1,8 @@
 from typing import Dict, Type, TypeVar
 from pygame.sprite import Group
+from rts.controllers.meta_singleton import MetaSingleton
 
 from rts.models.game_entity import GameEntity
-
-from threading import Lock
-
-
-class MetaSingleton(type):
-    """This is a thread-safe implementation of Singleton.
-
-    Returns:
-      Thread safe singleton.
-    """
-
-    _instances = {}
-    _lock: Lock = Lock()
-    """We now have a lock object that will be used to synchronize threads during
-    first access to the Singleton.
-  """
-
-    def __call__(cls, *args, **kwargs):
-        """
-        Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
-        """
-        with cls._lock:
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
-        return cls._instances[cls]
 
 
 class EntityController(metaclass=MetaSingleton):
