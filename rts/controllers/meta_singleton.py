e62f57746d1309bc2153a5a6a6534f4aa65a2133
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2022-05-10 00:23:09 +0200

    ref: add type to instances dict

--- rts/controllers/meta_singleton.py
+++ rts/controllers/meta_singleton.py
@@ -5,17 +5,14 @@ from weakref import WeakValueDictionary
 
 
 class MetaSingleton(type):
-    """This is a thread-safe implementation of Singleton.
+    """This is a thread-safe implementation of Singleton."""
 
-    Returns:
-      Thread safe singleton.
-    """
-
-    _instances = WeakValueDictionary()
+    _instances: WeakValueDictionary = WeakValueDictionary()
     _lock: Lock = Lock()
-    """We now have a lock object that will be used to synchronize threads during
+    """
+    We now have a lock object that will be used to synchronize threads during
     first access to the Singleton.
-  """
+    """
 
     def __call__(cls, *args, **kwargs):
         """
@@ -26,4 +23,5 @@ class MetaSingleton(type):
             if cls not in cls._instances:
                 instance = super().__call__(*args, **kwargs)
                 cls._instances[cls] = instance
+
         return cls._instances[cls]
