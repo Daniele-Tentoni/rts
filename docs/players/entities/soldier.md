c04565d7447a38cec1c48ade96c2a3b42a376cbd
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2023-09-07 00:18:09 +0200

    fix: introduce collision between soldiers
    This is a first step to the movement of soldiers to other towers. When encountering another soldier of an opponent owenership, they kill each other.

---
+++ docs/players/entities/soldier.md
@@ -0,0 +1,3 @@
+# Soldier
+
+The soldier is the most basic unit in the game. They fight for their rulers and kill enemiy ones.
--- tests/sprites/test_soldiers.py
+++ tests/sprites/test_soldiers.py
@@ -1,7 +1,10 @@
 import pytest
+from rts.controllers.entity_controller import EntityController
 from rts.models.game_entity import GameEntity
+from rts.sprites.ruler import Ruler
 
 from rts.sprites.soldier import Soldier
+from rts.sprites.tower import Tower
 
 
 @pytest.fixture
@@ -29,3 +32,33 @@ def test_simple_soldier_reset_initiative(sample_soldier: Soldier, delta: int):
     for i in range(0, 100):
         sample_soldier.update(delta=delta)
     assert abs(sample_soldier.initiative - 0.11) < 1e-16
+
+def test_collision_between_soldiers():
+    r1 = Ruler(GameEntity(0, 0, [0, 0, 0], [0, 0]), 1)
+    r2 = Ruler(GameEntity(0, 0, [0, 0, 0], [0, 0]), 1)
+    t1 = Tower(GameEntity(0, 0, [0, 0, 0], [0, 0]), r1, 1, [0, 0, 0], [0, 0], 0.1)
+    t2 = Tower(GameEntity(0, 0, [0, 0, 0], [0, 0]), r2, 1, [0, 0, 0], [0, 0], 0.1)
+    s1 = Soldier(GameEntity(0, 0, [0, 0, 0], [1, 1]), [0, 0], 0, t1, 0)
+    s2 = Soldier(GameEntity(0, 1, [0, 0, 0], [1, 1]), [0, 0], 0, t2, 0)
+    e = EntityController()
+    e.register_entity(s1)
+    e.register_entity(s2)
+    s1.update(1)
+    print(e.entity_dict[Soldier])
+    assert s1 in e.entity_dict[Soldier]
+    assert s2 in e.entity_dict[Soldier]
+    
+def test_collision_between_soldiers():
+    r1 = Ruler(GameEntity(0, 0, [0, 0, 0], [0, 0]), 1)
+    r2 = Ruler(GameEntity(0, 0, [0, 0, 0], [0, 0]), 1)
+    t1 = Tower(GameEntity(0, 0, [0, 0, 0], [0, 0]), r1, 1, [0, 0, 0], [0, 0], 0.1)
+    t2 = Tower(GameEntity(0, 0, [0, 0, 0], [0, 0]), r2, 1, [0, 0, 0], [0, 0], 0.1)
+    s1 = Soldier(GameEntity(0, 1, [0, 0, 0], [1, 1]), [0, 0], 0, t1, 0)
+    s2 = Soldier(GameEntity(0, 1, [0, 0, 0], [1, 1]), [0, 0], 0, t2, 0)
+    e = EntityController()
+    e.register_entity(s1)
+    e.register_entity(s2)
+    s1.update(1)
+    print(e.entity_dict[Soldier])
+    assert s1 not in e.entity_dict[Soldier]
+    assert s2 not in e.entity_dict[Soldier]
